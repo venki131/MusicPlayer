69a654ef5ab311a9a420cf7c5d75ca909957b089
venkatesh <venkatesh.n@techtreeit.com>
2019-05-07 17:39:01 +0530

    fetch files from storage

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.musicplayer;
 
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.IBinder;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private MusicPlayerService musicPlayerService;
     private boolean serviceBound = false;
+    private List<Audio> audioList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg");
+        loadAudio();
+        if (audioList.size() > 0)
+            playAudio(audioList.get(0).getData());
+        else
+            playAudio("http://mysound.ge/track/364/lady-gaga-amp-bradley-cooper-shallow");
     }
 
     //binding client to audio player service
@@ -43,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MusicPlayerService.class);
             intent.putExtra("media", media);
             startService(intent);
-            bindService(intent,serviceConnection,BIND_AUTO_CREATE);
+            bindService(intent, serviceConnection, BIND_AUTO_CREATE);
         }
     }
 
@@ -67,5 +79,32 @@ public class MainActivity extends AppCompatActivity {
             //service is active
             musicPlayerService.stopSelf();
         }
+    }
+
+    /**
+     * retrieve audio files from external storage
+     */
+    private void loadAudio() {
+        ContentResolver contentResolver = getContentResolver();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC + "!=0";
+        String sortOrder = MediaStore.Audio.Media.TITLE + "ASC";
+
+        Cursor cursor = contentResolver.query(uri, null, selection, null, sortOrder);
+        if (cursor != null && cursor.getCount() > 0) {
+            audioList = new ArrayList<>();
+            while (cursor.moveToNext()) {
+                String data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+                String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+
+                //save to audioList
+                audioList.add(new Audio(data, title, album, artist));
+            }
+        }
+        cursor.close();
+        Toast.makeText(this, "number of audio files = " + audioList.size(), Toast.LENGTH_LONG).show();
     }
 }
