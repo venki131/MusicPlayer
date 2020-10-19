01a8ba005df6cdac702f7fbc457193d059bf19ec
Suparna <suparnapai3156@gmail.com>
2020-10-19 17:35:16 +0530

    migrating project to Androidx

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -8,7 +8,7 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.IBinder;
 import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
